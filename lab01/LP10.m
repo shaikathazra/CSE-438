983df56e4b82bb788b0983908b3962c42ffe998c
SHAIKATHAZRA <shaikathazra@gmail.com>
2025-03-04 19:37:59 +0600

    hi

--- lab01/LP10.m
+++ lab01/LP10.m
@@ -10,3 +10,4 @@ I_negative =imcomplement(I);
 % Display the images
 subplot(1,2,1), imshow(I), title('Original Image');
 subplot(1,2,2), imshow(I_negative), title('Negative Image');
+%2020-1-60-072
